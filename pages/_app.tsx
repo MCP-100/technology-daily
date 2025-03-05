a25161e9bca8b996bf74ed379832c1e44aa92c21
king101125s@163.com <king101125s@gmail.com>
2025-03-06 03:27:22 +0800

    集成 Sentry 进行错误监控，添加相关配置文件和示例 API 路由

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,5 +1,13 @@
 import '../styles/globals.css'
 import type { AppProps } from 'next/app'
+import * as Sentry from "@sentry/nextjs"
+
+// Initialize Sentry
+Sentry.init({
+  dsn: process.env.NEXT_PUBLIC_SENTRY_DSN,
+  tracesSampleRate: 1.0,
+  debug: process.env.NODE_ENV === 'development',
+})
 
 export default function App({ Component, pageProps }: AppProps) {
   return <Component {...pageProps} />
